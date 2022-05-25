f634e093dfb0d1f30da881bab273f1d7c733d9d1
aselwilliams <aselwilliams22@gmail.com>
2022-05-25 10:20:49 -0400

    basic list search

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 import './App.css';
 import {Users} from './users';
 import {useState,useEffect} from 'react';
+import Table from './Table'
 
 function App() {
+  const [query,setQuery] =useState('');
+
+  const filterByName=()=>{
+   return Users.filter((user)=>user.first_name.toLowerCase().includes(query))
+  }
+ 
   return (
     <div className="App">
-      <input type='text' placeholder='Search...' className='search'/>
+      <input type='text' placeholder='Search...' className='search' onChange={(e)=>setQuery(e.target.value)}/>
       <ul className='list'>
-        <li className='listItem'>John</li>
+        {filterByName().map((user)=>(
+          <li className='listItem' key={user.id}>{user.first_name}</li>
+        ))}
       </ul>
     
     </div>
