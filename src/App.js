36f612a7589f5844608f9ae849d317a7dd8dc1ed
aselwilliams <aselwilliams22@gmail.com>
2022-05-25 10:06:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import {Users} from './users';
+import {useState,useEffect} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' placeholder='Search...' className='search'/>
+      <ul className='list'>
+        <li className='listItem'>John</li>
+      </ul>
+    
     </div>
   );
 }
