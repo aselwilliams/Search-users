60ff5525ab5a68d43fe62e3f379734333ac07a26
aselwilliams <aselwilliams22@gmail.com>
2022-05-25 10:32:34 -0400

    basic styles

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ import Table from './Table'
 function App() {
   const [query,setQuery] =useState('');
 
-  const filterByName=()=>{
+  const search=()=>{
    return Users.filter((user)=>user.first_name.toLowerCase().includes(query))
   }
  
   return (
-    <div className="App">
+    <div className="app">
       <input type='text' placeholder='Search...' className='search' onChange={(e)=>setQuery(e.target.value)}/>
-      <ul className='list'>
-        {filterByName().map((user)=>(
-          <li className='listItem' key={user.id}>{user.first_name}</li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     
     </div>
   );
 }
 
 export default App;
+
+{/* <ul className='list'>
+        {filterByName().map((user)=>(
+          <li className='listItem' key={user.id}>{user.first_name}</li>
+        ))}
+  </ul> */}
--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
-function Table() {
+function Table({data}) {
   return (
-    <div>Table</div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data.map((item)=>(
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.first_name}</td>
+                    <td>{item.last_name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.gender}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
   )
 }
 
